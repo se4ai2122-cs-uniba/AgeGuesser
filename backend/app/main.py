62bb5052e3d7d8c5ee6f36789d134d2c8f8193bc
onipot <m.sciancalepore20@studenti.uniba.it>
2021-11-30 14:28:38 +0100

    add missing url for cors. #21

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
     version="0.1",
 )
 
-origins = ["http://localhost:5000","http://localhost", "https://kub.k-stash.com/age", "https://age.kub.k-stash.com"]
+origins = ["http://localhost:5000","http://localhost","https://kub.k-stash.com", "https://kub.k-stash.com/age", "https://age.kub.k-stash.com"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -175,7 +175,7 @@ async def _post_models_age_predict(file: UploadFile = File(None, description="Im
         status=HTTPStatus.BAD_REQUEST
         )
      
-    print(model.name)
+    #print(model.name)
     model : EstimationModel = estimation_models[model.name]
     
     file_ = None

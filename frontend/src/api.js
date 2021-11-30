0e524f57dd20ea779c86705cba25e2630757eec3
onipot <m.sciancalepore20@studenti.uniba.it>
2021-11-30 14:17:46 +0100

    integrate backend apis #19 - models.age.predict - models.age.list

---
+++ frontend/src/api.js
@@ -0,0 +1,38 @@
+
+/*
+  payload: json containing the endpoint's parameters
+*/
+export async function upload(payload) {
+
+  const formData = new FormData();
+
+  formData.append("file", payload["file"]);
+  formData.append("model", payload["model"]);
+  formData.append("extract_faces", payload["extract_faces"]);
+
+  let res = await fetch("BASE_URL/models.age.predict", {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "*/*",
+      "Accept-Encoding": "gzip, deflate, br",
+    },
+  }).then((r) => r.json());
+
+  return res;
+}
+
+export async function getModels() {
+
+  let res = await fetch("BASE_URL/models.age.list", {
+    method: "GET",
+    headers: {
+      Accept: "*/*",
+      "Accept-Encoding": "gzip, deflate, br",
+    },
+  }).then((r) => r.json());
+
+  console.log(res.data)
+ 
+  return res;
+}

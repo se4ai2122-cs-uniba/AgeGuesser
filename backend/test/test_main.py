cb902af391a3401f292c4685c3621cfe6e84f681
onipot <m.sciancalepore20@studenti.uniba.it>
2022-01-02 14:04:34 +0100

    add new backend tests #38 - invalid base64-image - undefined input image - face detection endpoint

--- backend/test/test_main.py
+++ backend/test/test_main.py
@@ -1,10 +1,44 @@
 from http import HTTPStatus
-import json
 from fastapi.testclient import TestClient
 from app.main import app
+import pytest
 
+@pytest.fixture
+def image_file():
+    return {
+        'file': ('img.jpg', open('img.jpg', 'rb')),
+    }
+
+@pytest.fixture
+def image_base64():
+    with open("img_base64.jpg", "rb") as f:
+        return f.read()
+
+
+@pytest.fixture()
+def default_age_predict_payload():
+
+    payload = {
+                "model": "effnetv1_b0", 
+                "orientation": 0,
+                "extract_faces": True,
+                "img_base64": None
+            }
+    return payload
+
+@pytest.fixture()
+def default_face_predict_payload():
+
+    payload = {
+                "model": "yolov5s", 
+                "img_base64": None,
+                "threshold": 0.6
+            }
+    return payload
 
 with TestClient(app) as client:
+
+    
 
     def test_index():
         response = client.get("/")
@@ -22,67 +56,88 @@ with TestClient(app) as client:
          
         assert response.status_code == 200
         assert json_["message"] == "OK"
-        assert json_["method"] == "GET"    
-        assert json_["data"] == {"effnetv1_b0": {"name": "Age Estimation v1", "backbone": "EfficientNet-B0", "info": "MAE: 3 - STD: 2"}, "effnetv2_b0_torch": {"name": "Age Estimation v2", "backbone": "EfficientNetV2-B0", "info": "MAE: 3 - STD 1.3"}}
+        assert json_["method"] == "GET"
 
-    def test_model_age_predict():
-    
-        files= {
-            'file': ('img.jpg', open('img.jpg', 'rb')),
-        }
+        for model in json_["data"]:
+            model_info = json_["data"][model]
+            assert model_info.get("name") is not None
+            assert model_info.get("backbone") is not None
+            assert model_info.get("info") is not None
+
+    def test_model_age_predict(image_file, default_age_predict_payload):
         
-        multipart_form_data = {
-                "model": "effnetv1_b0", 
-                "orientation": 0,
-                "extract_faces": True
-            }
-
-        response = client.post("/models.age.predict", data=multipart_form_data, files=files)
+        response = client.post("/models.age.predict", data=default_age_predict_payload, files=image_file)
         
         json_ = response.json()
         
         assert response.status_code == 200
         assert len(json_["faces"]) == 6
     
-    def test_model_age_predict_base64():
+    def test_model_age_predict_base64(image_base64, default_age_predict_payload):
 
-        img = ""
-        with open("img_base64.jpg", "rb") as f:
-            img = f.read()
+        default_age_predict_payload["img_base64"] = image_base64
 
-        multipart_form_data = {
-                "model": "effnetv1_b0", 
-                "orientation": 0,
-                "extract_faces": True,
-                "img_base64": img
-            }
-
-        response = client.post("/models.age.predict", data=multipart_form_data, )
+        response = client.post("/models.age.predict", data=default_age_predict_payload, )
         
         json_ = response.json()
-        """ with open("result.json","w") as f:
-            json.dump(response.json(),f) """
+        
         assert response.status_code == 200
         assert len(json_["faces"]) == 6
     
-    def test_model_wrong_model():
+    def test_model_age_predict_base64_no_extract(image_base64, default_age_predict_payload):
 
-        img = ""
-        with open("img_base64.jpg", "rb") as f:
-            img = f.read()
+        default_age_predict_payload["img_base64"] = image_base64
+        default_age_predict_payload["extract_faces"] = False
+        response = client.post("/models.age.predict", data=default_age_predict_payload, )
+        
+        json_ = response.json()
+        
+        assert response.status_code == 200
+        assert len(json_["faces"]) == 1
+    
+    def test_model_age_predict_invalid_base64(default_age_predict_payload):
 
-        multipart_form_data = {
-                "model": "effnetv1_bO_with_typo", 
-                "orientation": 0,
-                "extract_faces": True,
-                "img_base64": img
-            }
+        image_base64 = "somedummybase64"
 
-        response = client.post("/models.age.predict", data=multipart_form_data, )
+        default_age_predict_payload["img_base64"] = image_base64
+
+        response = client.post("/models.age.predict", data=default_age_predict_payload, )
+        
+        json_ = response.json()
+        assert response.status_code == 200
+        assert len(json_["faces"]) == 0
+        assert "invalid" in json_["message"].lower()
+        assert json_["status"] == HTTPStatus.BAD_REQUEST
+    
+    def test_model_wrong_model(default_age_predict_payload):
+
+        default_age_predict_payload["model"] = "effnetv1_bO_with_typo"
+
+        response = client.post("/models.age.predict", data=default_age_predict_payload, )
         
         json_ = response.json()
         
         assert json_["status"] == HTTPStatus.BAD_REQUEST
         assert len(json_["faces"]) == 0
         assert json_["message"] =="Unknown model. Please look at the available ones at /models.age.list"
+    
+    def test_model_face_predict_base64(image_base64, default_face_predict_payload):
+
+        default_face_predict_payload["img_base64"] = image_base64
+
+        response = client.post("/models.face.predict", data=default_face_predict_payload, )
+        
+        json_ = response.json()
+        
+        assert response.status_code == 200
+        assert len(json_["faces"]) == 6
+    
+    def test_model_face_predict_file(image_file, default_face_predict_payload):
+
+        response = client.post("/models.face.predict", data=default_face_predict_payload, files=image_file)
+        
+        json_ = response.json()
+        
+        assert response.status_code == 200
+        assert len(json_["faces"]) == 6
     
